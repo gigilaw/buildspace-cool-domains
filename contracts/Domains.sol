de4f9bda8439b535ac0e9b274858605a72e9e981
Gigi Law <gigi.ht.law.pccw@gmail.com>
2022-03-21 12:04:07 +0800

    map domains to sender address

---
+++ contracts/Domains.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+  mapping(string => address) public domains;
+
+  constructor() {
+      console.log("meow~.");
+  }
+
+  function register(string calldata name) public {
+      domains[name] = msg.sender;
+      console.log("%s has registered a domain!", msg.sender);
+  }
+
+  function getAddress(string calldata name) public view returns (address) {
+      return domains[name];
+  }
+}
