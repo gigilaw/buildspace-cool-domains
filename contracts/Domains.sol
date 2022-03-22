be3f93fdf189e65834b4d3a568b2dca6642b2038
Gigi Law <gigi.ht.law.pccw@gmail.com>
2022-03-22 11:40:59 +0800

    test TLD and payment for domain

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -3,20 +3,52 @@
 pragma solidity ^0.8.10;
 
 import "hardhat/console.sol";
+import { StringUtils } from "./libraries/StringUtils.sol";
 
 contract Domains {
-  mapping(string => address) public domains;
+  string public tld;
 
-  constructor() {
-      console.log("meow~.");
+  mapping(string => address) public domains;
+  mapping(string => string) public records;
+
+  constructor(string memory _tld) payable {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
   }
 
-  function register(string calldata name) public {
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**10; 
+    } else if (len == 4) {
+      return 3 * 10**10; 
+    } else {
+      return 1 * 10**10;
+    }
+  }
+
+  function register(string calldata name) public payable{
+      require(domains[name] == address(0));
+      uint _price = price(name);
+
+      require(msg.value >= _price, "Not enough Matic paid");
+      
       domains[name] = msg.sender;
       console.log("%s has registered a domain!", msg.sender);
   }
 
   function getAddress(string calldata name) public view returns (address) {
       return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+      // Check that the owner is the transaction sender
+      require(domains[name] == msg.sender);
+      records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns(string memory) {
+      return records[name];
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,17 +1,20 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners()
   const domainContractFactory = await hre.ethers.getContractFactory('Domains')
-
-  const domainContract = await domainContractFactory.deploy()
+  const domainContract = await domainContractFactory.deploy('Frisbee')
   await domainContract.deployed()
-  console.log('Contract deployed to:', domainContract.address)
-  console.log('Contract deployed by:', owner.address)
 
-  const txn = await domainContract.register('frisbee')
+  console.log('Contract deployed to:', domainContract.address)
+
+  let txn = await domainContract.register('star', {
+    value: hre.ethers.utils.parseEther('0.1'),
+  })
   await txn.wait()
 
-  const domainOwner = await domainContract.getAddress('frisbee')
-  console.log('Owner of domain:', domainOwner)
+  const address = await domainContract.getAddress('star')
+  console.log('Owner of domain star:', address)
+
+  const balance = await hre.ethers.provider.getBalance(domainContract.address)
+  console.log('Contract balance:', hre.ethers.utils.formatEther(balance))
 }
 
 const runMain = async () => {
